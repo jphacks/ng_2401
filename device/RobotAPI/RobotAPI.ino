14b0010980226970ba84c74576755044a57153c8
HarumaIto-loovic <haruma.ito@loovic.co.jp>
2024-10-26 14:28:00 +0900

    add: BLEを実装

--- device/RobotAPI/RobotAPI.ino
+++ device/RobotAPI/RobotAPI.ino
@@ -1,12 +1,18 @@
-#include <WiFiManager.h>
-#include <WiFiClientSecure.h>
-#include <ESPmDNS.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 #include <ESP32Servo.h>
 
-// API用
-WiFiClientSecure client;
-WiFiManager wifiManager;
-WiFiServer server(80);
+// BLE用
+#define SERVICE_UUID        "55725ac1-066c-48b5-8700-2d9fb3603c5e"
+#define CHARACTERISTIC_UUID "69ddb59c-d601-4ea4-ba83-44f679a670ba"
+#define BLE_DEVICE_NAME     "WriteRobot"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
 // Servo用
 Servo servoRight;
@@ -18,6 +24,33 @@ int period = 20000;
 int minUs = 550;
 int maxUs = 2450;
 
+// BLE Utils
+class ServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    deviceConnected = true;
+    Serial.println("onConnect");
+  };
+  void onDisconnect(BLEServer *pServer) {
+    deviceConnected = false;
+    Serial.println("onDisconnect");
+  };
+};
+
+class CharacteristicCallbacks: public BLECharacteristicCallbacks {
+  // Writeされた時
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    Serial.println("onWrite");
+    String value = pCharacteristic->getValue();
+    if (value.length() > 0) {
+      for (int i = 0; i < value.length(); i++) {
+        Serial.print("Received Value: ");
+        Serial.print(value[i], HEX);
+      }     
+      Serial.println();
+    }
+  }
+};
+
 void setup() {
   Serial.begin(115200);
 
@@ -26,27 +59,50 @@ void setup() {
   servoLeft.setPeriodHertz(period);
   servoRight.attach(rightPin, minUs, maxUs);
   servoLeft.attach(leftPin, minUs, maxUs);
+
+  BLEDevice::init(BLE_DEVICE_NAME);
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_WRITE  |
+    BLECharacteristic::PROPERTY_NOTIFY
+  );
+
+  pCharacteristic->setCallbacks(new CharacteristicCallbacks());
+  pCharacteristic->addDescriptor(new BLE2902());
+
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);
+  BLEDevice::startAdvertising();
+
+  Serial.println("End Setup");
   
   // 初期値は開いている状態にする
   servoRight.write(0);
   servoLeft.write(180);
+
+  // 安定するまで待つ
   delay(1000);
 }
 
 void loop() {
-  int pos = 0;
-  
-  for (pos = 0; pos <= 90; pos++) {
-    servoRight.write(pos);
-    servoLeft.write(180-pos);
-    delay(2);
+  // disconnecting
+  if(!deviceConnected && oldDeviceConnected){
+    delay(500); // give the bluetooth stack the chance to get things ready
+    pServer->startAdvertising();
+    Serial.println("restartAdvertising");
+    oldDeviceConnected = deviceConnected;
   }
-  delay(1000);
-  for (pos = 90; pos >= 0; pos--) {
-    servoRight.write(pos);
-    servoLeft.write(180-pos);
-    delay(2);
+  // connecting
+  if(deviceConnected && !oldDeviceConnected){
+    oldDeviceConnected = deviceConnected;
   }
-  delay(1000);
-  
 }
